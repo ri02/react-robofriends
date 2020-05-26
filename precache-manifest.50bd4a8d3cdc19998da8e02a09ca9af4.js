5af66479ec51239dc48e5eca122c6d9b138ddd2c
Marie Hess <hess.marietheres@gmail.com>
2020-05-26 12:12:11 +0200

    Updates

---
+++ precache-manifest.50bd4a8d3cdc19998da8e02a09ca9af4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73e0ad5fb5cd289807af6c31818332f7",
+    "url": "/react-robofriends/index.html"
+  },
+  {
+    "revision": "5e4b88221347fc7985ae",
+    "url": "/react-robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6c6c8c022e97b8155da5",
+    "url": "/react-robofriends/static/css/main.2ed3648d.chunk.css"
+  },
+  {
+    "revision": "5e4b88221347fc7985ae",
+    "url": "/react-robofriends/static/js/2.dede1e61.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-robofriends/static/js/2.dede1e61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c6c8c022e97b8155da5",
+    "url": "/react-robofriends/static/js/main.713a8deb.chunk.js"
+  },
+  {
+    "revision": "0fe8a51e591504870130",
+    "url": "/react-robofriends/static/js/runtime-main.abb0a2bc.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/react-robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
